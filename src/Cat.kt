d87a996089eaa4e6511171dfa8969344349e7e65
nick <gavrilutageoge@gmail.com>
2025-10-25 16:10:46 +0100

    change the cat method weight that it is not going above 20

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,12 +1,27 @@
 class Cat (val name: String, var age: Int, var weight: Int) {
 
-    fun walk(times:  Int = 1) {
+    /*fun walk(times:  Int = 1) {
         weight-= times
+    }*/
+    fun walk(times: Int) {
+        weight -= times
+        if (weight < 0) {
+            weight = 0
+            println("$name has walked too far and lost all its weight!")
+        }
     }
 
     // 🐟 New method: increases the cat’s weight by 1
-    fun eat(times: Int = 1) {
+    /*fun eat(times: Int = 1) {
         weight+= times
+    }*/
+
+    fun eat() {
+        if (weight >= 20) {
+            println("Error: $name cannot eat anymore — already at maximum weight (20).")
+        } else {
+            weight++
+        }
     }
 
     override fun toString() : String {
--- src/main.kt
+++ src/main.kt
@@ -5,14 +5,14 @@ fun main() {
     val cupra = Cat("Cupra", 2, 7)
 
     binnie.walk(2)
-    clyde.walk(4)
+    clyde.walk(3)
 
-    binnie.eat(3)
-    clyde.eat(4)
+    binnie.eat()
+    clyde.eat()
     flatHead.walk(5)
     cupra.walk(2)
-    flatHead.eat(4)
-    cupra.eat(3)
+    flatHead.eat()
+    cupra.eat()
 
     println(binnie)
     println(clyde)
